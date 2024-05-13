11a9ec2ac094d505d02f26cbef2ae596b2aee971
scardona14 <cardanasean123@gmail.com>
2024-05-13 12:55:49 -0400

    Fixing with semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = argv[2];
 const string = argv[3];
 fs.writeFile(filePath, string, 'utf8', function (err) {
-    if (err) {
-        return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 });
