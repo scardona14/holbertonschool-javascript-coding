fa82dd95f5fea3da82da7c67aa56b45dd68caa8c
scardona14 <cardanasean123@gmail.com>
2024-05-13 14:41:29 -0400

    Fixing semistandard

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const argv = process.argv;
 const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2] + '/';
 
 request(url, function (error, response, body) {
-    if (error === null) {
-        console.log(JSON.parse(body).title);
-    } else {
-        console.log(error);
-    }
+  if (error === null) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log(error);
+  }
 }); // Add closing parenthesis here
