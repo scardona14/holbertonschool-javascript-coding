b80e85b38ff11e25ab6d011a4755ef1a491aa258
scardona14 <cardanasean123@gmail.com>
2024-05-14 11:32:40 -0400

    Writing a script that computes the number of tasks completed by user id.

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,18 +3,18 @@ const argv = process.argv;
 const url = argv[2];
 const request = require('request');
 request(url, function (error, response, body) {
-    if (error === null) {
-        const tasks = JSON.parse(body);
-        const dict = {};
-        for (const task of tasks) {
-            if (task.completed === true) {
-                if (dict[task.userId] === undefined) {
-                    dict[task.userId] = 1;
-                } else {
-                    dict[task.userId]++;
-                }
-            }
-            console.log(dict);
+  if (error === null) {
+    const tasks = JSON.parse(body);
+    const dict = {};
+    for (const task of tasks) {
+      if (task.completed === true) {
+        if (dict[task.userId] === undefined) {
+          dict[task.userId] = 1;
+        } else {
+          dict[task.userId]++;
         }
+      }
+      console.log(dict);
     }
+  }
 });
